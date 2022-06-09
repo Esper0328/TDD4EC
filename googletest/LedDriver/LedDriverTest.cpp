15edbfc0aa061c663dc82d54def3b0a36cb60429
Esper0328 <esper0328@gmail.com>
2022-06-09 10:44:45 +0900

    refactored LedDriverTest by using Test Fixture

--- googletest/LedDriver/LedDriverTest.cpp
+++ googletest/LedDriver/LedDriverTest.cpp
@@ -1,32 +1,33 @@
 #include <stdint.h>
 
-extern "C"
-{
+extern "C" {
 #include "LedDriver.h"
 }
 
 #include <gtest/gtest.h>
 
-TEST(LedDriver, LedsOffAfterCreate)
-{
-    uint16_t virtualleds = 0xffff;
+class LedDriverTest : public ::testing::Test {
+    protected:
+        uint16_t virtualleds;
+        virtual void SetUp() { LedDriver_Create(&virtualleds); }
+};
+
+TEST_F(LedDriverTest, LedsOffAfterCreate) {
+    virtualleds = 0xffff;
     LedDriver_Create(&virtualleds);
     ASSERT_EQ(0, virtualleds);
 }
 
-TEST(LedDriver, TurnOnLedOne)
-{
-    uint16_t virtualleds = 0;
-    LedDriver_Create(&virtualleds);
+TEST_F(LedDriverTest, TurnOnLedOne) {
+    virtualleds = 0;
     LedDriver_TurnOn(1);
     ASSERT_EQ(1, virtualleds);
 }
 
-TEST(LedDriver, TurnOffLedOne)
-{
-    uint16_t virtualleds;
-    LedDriver_Create(&virtualleds);
+TEST_F(LedDriverTest, TurnOffLedOne) {
+    virtualleds = 0;
     LedDriver_TurnOn(1);
     LedDriver_TurnOff(1);
     ASSERT_EQ(0, virtualleds);
 }
+
